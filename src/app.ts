663edb8e2977b0526d0b551fe3d7bb5bbe27b683
sgayathrii <sgayathrii@gmail.com>
2023-12-08 12:27:08 +0100

    Fix: backend connection

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
-import express from 'express';
+import express, { Request, Response, Application } from 'express';
 import cors from "cors";
 import dotenv from 'dotenv';
 
 // For env File
 dotenv.config();
 
-//import userRoutes from './routes/userRoutes';
-//import campingRoutes from './routes/campingRoutes';
+import userRoutes from './routes/userRoutes';
+import campingRoutes from './routes/campingRoutes';
 
-const app = express();
+const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
-//app.use('/api/users', userRoutes);
-//app.use('/api/camping_areas', campingRoutes); 
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Express & TypeScript Server');
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/camping_areas', campingRoutes); 
 
 export default app;
